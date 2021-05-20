fe27142744c3f48f3c24fd138b014e5e6addc8bb
Bre Thunholm <bthunholm@gmail.com>
2021-05-19 19:15:22 -1000

    added coutdown.css

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,41 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
+import './countdown.css';
+// import styled from 'styled-components';
 
-const StyledCountdown = styled.div`
-  height: 30vh;
-  width: 100vw;
-  padding: 50px;
+// const StyledCountdown = styled.div`
+//   .countdown {
+//     height: 30vh;
+//     width: 100vw;
+//     padding: 50px;
+//     display: flex;
+//     gap: 20px;
+//     justify-content: center;
+//     align-items: center;
+//     align-content: center;
+//     font-size: 35px;
+//   }
 
-  .countdown {
-    display: flex;
-    gap: 20px;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    font-size: 35px;
-  }
-
-  .highlight {
-    color: #bcbdc2;
-  }
-`;
+//   .highlight {
+//     color: #bcbdc2;
+//   }
+// `;
 
 export default function Countdown() {
   return (
-    <StyledCountdown>
-      <div className="countdown">
-        <p>
-          <span className="highlight">04</span> Months
-        </p>
-        <p>
-          {' '}
-          <span className="highlight">22</span> days
-        </p>
-        <p>
-          {' '}
-          <span className="highlight">03</span> Hours
-        </p>
-      </div>
-    </StyledCountdown>
+    <div className="countdown">
+      <p>
+        <span className="highlight">04</span> Months
+      </p>
+      <p>
+        {' '}
+        <span className="highlight">22</span> days
+      </p>
+      <p>
+        {' '}
+        <span className="highlight">03</span> Hours
+      </p>
+    </div>
   );
 }
