e68822b5c8a913b6edfbc33e55b5516015ef63a2
Bre Thunholm <bthunholm@gmail.com>
2021-05-19 19:35:00 -1000

    updated rsvp form css

--- src/components/rsvp.js
+++ src/components/rsvp.js
@@ -5,33 +5,35 @@ import styled from 'styled-components';
 const StyledRsvp = styled.div`
   @import url('https://fonts.googleapis.com/css?family=Old+Standard+TT:400i|Rubik" rel="stylesheet');
 
+  height: 90vh;
+  width: 100%;
   display: grid;
-  justify-content: center;
+  justify-items: center;
+  align-items: center;
 
-  .background {
-    height: 90vh;
-    width: 100%;
-    display: grid;
-    justify-items: center;
-    align-items: center;
-    /* position: relative; */
-    background-size: cover;
-    background-image: url('images/floral-background.jpg');
+  position: relative;
+  background-size: cover;
+  background-image: url('images/floral-background.jpg');
+
+  .center {
+    text-align: center;
   }
 
   .rsvp-container {
     height: 90%;
     width: 60%;
-    border: solid red 2px;
     background: white;
     padding: 50px;
     display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
     justify-content: center;
     align-items: center;
   }
 
   .form {
     display: grid;
+    justify-content: center;
     gap: 20px;
     grid-template-rows: 1fr 1fr 1fr 1fr;
   }
@@ -40,28 +42,26 @@ const StyledRsvp = styled.div`
 export default function Rsvp() {
   return (
     <StyledRsvp>
-      <div className="background">
-        <div className="rsvp-container">
-          <span>RSVP</span>
-          <h1>We are so excited to celebrate with you!</h1>
-          <h3>Kindly respond by September 1st 2021</h3>
-          <form className="form">
-            <input type="text" placeholder="email" />
-            <input type="text" placeholder="name" />
-            <label>
-              Are you attending?
-              <input type="text" />
-            </label>
-            <label>
-              Who is joining you?
-              <input type="text" />
-            </label>
-            <label>
-              Message:
-              <input type="text" />
-            </label>
-          </form>
-        </div>
+      <div className="rsvp-container">
+        <p className="center">RSVP</p>
+        <h1 className="center">We are so excited to celebrate with you!</h1>
+        <h3 className="center">Kindly respond by September 1st 2021</h3>
+        <form className="form">
+          <input type="text" placeholder="email" />
+          <input type="text" placeholder="name" />
+          <label>
+            Are you attending?
+            <input type="text" />
+          </label>
+          <label>
+            Who is joining you?
+            <input type="text" />
+          </label>
+          <label>
+            Message:
+            <input type="text" />
+          </label>
+        </form>
       </div>
     </StyledRsvp>
   );
